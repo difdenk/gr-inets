36ea6f7d3acc7e408cfdb1259e35ad8ff6ad1978
Can Ercandogu <cn.ercandogu@gmail.com>
2017-08-24 12:57:14 +0200

    frame_check block now can calculate bit error rate

--- lib/frame_check_impl.h
+++ lib/frame_check_impl.h
@@ -1,17 +1,17 @@
 /* -*- c++ -*- */
-/* 
+/*
  * Copyright 2017 <+YOU OR YOUR COMPANY+>.
- * 
+ *
  * This is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3, or (at your option)
  * any later version.
- * 
+ *
  * This software is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this software; see the file COPYING.  If not, write to
  * the Free Software Foundation, Inc., 51 Franklin Street,
@@ -33,12 +33,17 @@ namespace gr {
      private:
       int _develop_mode;
       int _block_id;
-      int _frame_type; 
-      boost::crc_optimal<32, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true> _crc_impl; 
+      int _frame_type;
+      boost::crc_optimal<32, 0x04C11DB7, 0xFFFFFFFF, 0xFFFFFFFF, true, true> _crc_impl;
       pmt::pmt_t crc32_bb_calc(pmt::pmt_t msg);
       void check_frame(pmt::pmt_t frame_info);
       void check_frame_v0(pmt::pmt_t rx_frame);
       void disp_vec(std::vector<unsigned char> vec);
+      std::vector<uint8_t> generate();
+      std::vector<uint8_t> _reference_payload;
+      int _error;
+      int _success;
+      void ber_calculate(std::vector<unsigned char> payload_vector);
 
      public:
       frame_check_impl(int develop_mode, int block_id);
@@ -49,4 +54,3 @@ namespace gr {
 } // namespace gr
 
 #endif /* INCLUDED_INETS_FRAME_CHECK_IMPL_H */
-
